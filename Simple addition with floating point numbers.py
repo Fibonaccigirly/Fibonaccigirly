d28d23ab6402292a3e18eebc2fc56cb969c96a64
Fibonaccigirly <152340015+Fibonaccigirly@users.noreply.github.com>
2023-12-11 17:36:56 -0800

    Add files via upload

---
+++ Simple addition with floating point numbers.py
@@ -0,0 +1,15 @@
+# Simple Addition 
+print("Hello! I'm your math robot, June!")
+
+# Collecting two numbers
+first_number = float(input("Type a number and hit enter: "))
+second_number = float(input("Type another number and hit enter: "))
+
+# Adding the inputs 
+sum_result = first_number + second_number
+
+# Revealing the answer 
+print(f"{first_number} + {second_number} = {sum_result}")
+
+# Say our goodbyes
+print("Thank you, have a nice day!")
